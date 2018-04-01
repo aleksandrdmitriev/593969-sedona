652a21b07335fcbb0c707b584f2b228b097109bd
aleksandrdmitriev <aleksandrdmitriev@users.noreply.github.com>
2018-04-01 19:34:49 +0300

    минификация js

--- js/search-form.js
+++ js/search-form.js
@@ -1,9 +1,8 @@
         var link = document.querySelector(".modal-close-open");
-        
         var popup = document.querySelector(".modal-find");
                
           link.addEventListener("click", function (evt) {
           evt.preventDefault();
           popup.classList.toggle("modal-show");
-           });
+          });
     
