e5fee142592d836bfc918cf2b8b48de8a9bdf69a
aleksandrdmitriev <aleksandrdmitriev@users.noreply.github.com>
2018-04-03 15:28:55 +0300

    открытие формы без JS

--- js/search-form.js
+++ js/search-form.js
@@ -2,8 +2,10 @@ var link = document.querySelector(".modal-close-open");
 var popup = document.querySelector(".modal-find");
 var arrival = popup.querySelector("[name=arrival]");
 
+popup.classList.add("modal-hide");
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.toggle("modal-show");
+  popup.classList.toggle("modal-hide");
   arrival.focus();
 });
